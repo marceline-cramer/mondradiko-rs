021149b4bd771a1f5f11bf6345d9ce7bb18db0f8
marceline-cramer <cramermarceline@gmail.com>
2021-12-11 19:50:02 -0700

    Add BasicCoreApi

--- mondradiko-core/src/lib.rs
+++ mondradiko-core/src/lib.rs
@@ -77,10 +77,62 @@ impl Core {
             }
         }
     }
+}
 
-    /// Temporary script API callback
-    pub fn test_cb(&self) {
-        let mut positions = self.positions.borrow_mut();
-        positions.push(PositionComponent { position: [0.0; 3] });
+pub struct BasicCoreApi {
+    core: Arc<Core>,
+}
+
+impl BasicCoreApi {
+    pub fn new(core: Arc<Core>) -> Self {
+        Self { core }
+    }
+}
+
+impl ScriptApi for BasicCoreApi {
+    fn load_asset(
+        &mut self,
+        asset_type: AssetType,
+        id: AssetId,
+        data: &[u8],
+    ) -> ScriptResult<AssetId> {
+        unimplemented!()
+    }
+
+    fn get_asset_type(&self, name: &str) -> ScriptResult<AssetType> {
+        unimplemented!()
+    }
+
+    fn write_components(&mut self, component: ComponentId, entities: &[EntityId], data: &[u8]) {
+        // TODO implement component registry
+        assert_eq!(component.0, 0);
+
+        // TODO actually insert by entity id
+        // TODO cast to different component types
+        let positions: &[PositionComponent] = bytemuck::cast_slice(data);
+        self.core
+            .positions
+            .borrow_mut()
+            .extend_from_slice(positions);
+    }
+
+    fn delete_components(&mut self, component: ComponentId, entities: &[EntityId]) {
+        unimplemented!()
+    }
+
+    fn get_resource_id(&self, name: &str) -> ScriptResult<ResourceId> {
+        unimplemented!()
+    }
+
+    fn write_resource(&mut self, id: ResourceId, data: &[u8]) {
+        unimplemented!()
+    }
+
+    fn get_event_type(&self, name: &str) -> ScriptResult<EventType> {
+        unimplemented!()
+    }
+
+    fn register_event_handler(&mut self, event_type: EventType, handler: EventHandler) {
+        unimplemented!()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use mondradiko_core::*;
+use mondradiko_types::*;
 use std::sync::Arc;
 
-struct ExampleInstance {
-    core: Arc<Core>,
+struct ExampleInstance<T> {
+    api: T,
 }
 
-impl ScriptInstance for ExampleInstance {
+impl<T: ScriptApi> ScriptInstance for ExampleInstance<T> {
     fn handle_event(&mut self, event_type: EventType, data: &[u8]) {
-        self.core.test_cb();
+        let position = components::PositionComponent { position: [0.0; 3] };
+        let positions = &[position];
+        let components: &[u8] = bytemuck::cast_slice(positions);
+        self.api
+            .write_components(ComponentId(0), &[EntityId(0)], components);
     }
 }
 
@@ -16,7 +21,7 @@ fn main() {
     let mut core = Arc::new(Core::default());
 
     let script = ExampleInstance {
-        core: core.to_owned(),
+        api: BasicCoreApi::new(core.clone()),
     };
     core.add_script(Box::new(script));
 
