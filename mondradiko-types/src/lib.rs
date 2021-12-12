b29fabacd1f52e6def368053258af9770e031e60
marceline-cramer <cramermarceline@gmail.com>
2021-12-11 21:35:32 -0700

    Add serde and improve derive usage

--- mondradiko-types/src/lib.rs
+++ mondradiko-types/src/lib.rs
@@ -1,23 +1,28 @@
 pub use bytemuck;
 
-#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, bytemuck::Pod, bytemuck::Zeroable)]
+use bytemuck::{Pod, Zeroable};
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, Pod, Zeroable)]
 #[repr(transparent)]
 pub struct AssetId(pub u32);
 
-#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, bytemuck::Pod, bytemuck::Zeroable)]
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, Pod, Zeroable)]
 #[repr(transparent)]
 pub struct AssetType(pub u32);
 
-#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, bytemuck::Pod, bytemuck::Zeroable)]
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, Pod, Zeroable)]
 #[repr(transparent)]
 pub struct ComponentId(pub u32);
 
-#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, bytemuck::Pod, bytemuck::Zeroable)]
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, Pod, Zeroable)]
 #[repr(transparent)]
 pub struct EntityId(pub u32);
 
 pub mod events {
-    #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, bytemuck::Pod, bytemuck::Zeroable)]
+    use super::*;
+
+    #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, Pod, Zeroable)]
     #[repr(C)]
     pub struct UpdateEvent {
         // TODO delta-time member
@@ -27,30 +32,51 @@ pub mod events {
 pub mod components {
     use super::*;
 
-    #[derive(Clone, Copy, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+    #[derive(Clone, Copy, Debug, Pod, Zeroable)]
     #[repr(C)]
-    pub struct PositionComponent {
+    pub struct Position {
         pub position: [f32; 3],
     }
 
-    #[derive(Clone, Copy, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+    #[derive(Clone, Copy, Debug, Pod, Zeroable)]
     #[repr(C)]
-    pub struct MeshComponent {
+    pub struct Label {
+        pub label: AssetId,
+    }
+
+    #[derive(Clone, Copy, Debug, Pod, Zeroable)]
+    #[repr(C)]
+    pub struct Mesh {
         pub mesh: AssetId,
     }
 }
 
 pub mod assets {
+    use super::*;
+
     pub mod mesh {
+        use super::*;
+
+        #[derive(Clone, Debug, Deserialize, Serialize)]
         pub struct Vertex {
             pub position: [f32; 3],
         }
 
         pub type Index = u32;
 
+        #[derive(Clone, Debug, Deserialize, Serialize)]
         pub struct MeshAsset {
             pub vertices: Vec<Vertex>,
             pub indices: Vec<Index>,
+        }
+    }
+
+    pub mod label {
+        use super::*;
+
+        #[derive(Clone, Debug, Deserialize, Serialize)]
+        pub struct LabelAsset {
+            pub title: String,
         }
     }
 }
